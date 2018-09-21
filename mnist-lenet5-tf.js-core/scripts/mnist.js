829cd1eea604f84c68ee284117b768244fd4114d
David Xiang <xiangdongwei@pku.edu.cn>
2018-09-21 18:15:08 +0800

    update lenet5 tf-python edtion

---
+++ mnist-lenet5-tf.js-core/scripts/mnist.js
@@ -0,0 +1,165 @@
+/*
+author: David Xiang
+email: xdw@pku.edu.cn
+ */
+console.log(tf.getBackend());
+//tf.setBackend("cpu");
+var btn = document.getElementById("train");
+var nConv = document.getElementById("nConv");
+var nPool = document.getElementById("nPool");
+var equation = document.getElementById("equation");
+var mPerform = document.getElementById("mPerform");
+var mInfo, tInfo;
+
+const LEARNING_RATE = 0.15;
+const optimizer = tf.train.sgd(LEARNING_RATE);
+const BATCH_SIZE = 64;
+const TRAIN_BATCHES = 1500;
+const TEST_BATCH_SIZE = 10000;
+const TEST_ITERATION_FREQUENCY = 5;
+
+async function train(nconv, npool){
+    const model = tf.sequential();
+    model.add(tf.layers.conv2d({
+        inputShape: [28, 28, 1],
+        kernelSize: 5,
+        filters: 6,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: tf.initializers.truncatedNormal({stddev:0.1}),
+        padding: 'same',
+        useBias: true,
+        biasInitializer: tf.initializers.constant({value:0.0})
+    }));
+
+    model.add(tf.layers.maxPooling2d({
+        poolSize: [2, 2],
+        strides: [2, 2],
+        padding: 'same'
+    }));
+
+    model.add(tf.layers.conv2d({
+        inputShape: [14, 14, 6],
+        kernelSize: 5,
+        filters: 16,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: tf.initializers.truncatedNormal({stddev:0.1}),
+        padding: 'valid',
+        useBias: true,
+        biasInitializer: tf.initializers.constant({value:0.0})
+    }));
+
+    model.add(tf.layers.maxPooling2d({
+        poolSize: [2, 2],
+        strides: [2, 2],
+        padding: 'same'
+    }));
+
+    model.add(tf.layers.flatten({
+        inputShape: [5, 5, 16]
+    }));
+
+    model.add(tf.layers.dense({
+        inputShape: [400],
+        units: 120,
+        activation: "relu",
+        useBias: true,
+        biasInitializer: tf.initializers.constant({value:0.0}),
+        kernelInitializer: tf.initializers.truncatedNormal({stddev:0.1}),
+        kernelRegularizer: tf.regularizers.l2()
+    }));
+
+    model.add(tf.layers.dense({
+        inputShape: [120],
+        units: 84,
+        activation: "relu",
+        useBias: true,
+        biasInitializer: tf.initializers.constant({value:0.0}),
+        kernelInitializer: tf.initializers.truncatedNormal({stddev:0.1}),
+        kernelRegularizer: tf.regularizers.l2()
+    }));
+
+    model.add(tf.layers.dense({
+        units: 10,
+        kernelInitializer: tf.initializers.truncatedNormal({stddev:0.1}),
+        activation: "softmax"
+    }));
+
+    model.compile({
+        optimizer: optimizer,
+        loss: "categoricalCrossentropy",
+        metrics: ['accuracy']
+    });
+
+    statusLog("Training");
+
+    for (let i = 0; i < TRAIN_BATCHES; i++){
+        const [batch, validationData] = tf.tidy(()=>{
+            const batch = data.nextTrainBatch(BATCH_SIZE);
+            batch.xs = batch.xs.reshape([BATCH_SIZE, 28, 28, 1]);
+
+            let validationData;
+            if (i % TEST_ITERATION_FREQUENCY === 0){
+                const testBatch = data.nextTestBatch(TEST_BATCH_SIZE);
+                validationData = [
+                    testBatch.xs.reshape([TEST_BATCH_SIZE, 28, 28, 1]), testBatch.labels
+                ];
+            }
+            return [batch, validationData];
+        });
+
+        let history = await model.fit(
+            batch.xs, 
+            batch.labels,
+            {batchSize: BATCH_SIZE, validationData, epochs: 1});
+
+        const loss = history.history.loss[0];
+        const accuracy = history.history.acc[0];
+
+        if (validationData != null)
+            infoLog('Batch #' + i + "    Loss: " + loss.toFixed(3) + 
+                "    Accuracy: " + accuracy.toFixed(3));
+        else
+            infoLog('Batch #' + i + "    Loss: " + loss.toFixed(3));
+
+        // get memory performance
+        if (i == parseInt(TRAIN_BATCHES / 2)){
+            mInfo = tf.memory();
+            //console.log(JSON.stringify(mInfo));
+        }
+
+        tf.dispose([batch, validationData]);
+        //let info2 = await tf.time(async()=>{
+            await tf.nextFrame();
+            //});
+        //console.log(JSON.stringify(info));
+    }
+}
+
+
+let data;
+async function load(){
+    data = new MnistData();
+    await data.load();
+    statusLog("Ready");
+}
+
+btn.onclick = async function(){
+    nconv = parseInt(nConv.value);
+    npool = parseInt(nPool.value);
+    equation.innerText = "total num of layers: (" + nconv + " + 1) * "
+        + npool + " + 2 = " + ((nconv + 1) * npool + 2);
+    
+    console.log("start training");
+    console.time("train");
+    await train(nconv, npool);
+    console.timeEnd("train");
+
+    mPerform.innerText = "memory performance: " + JSON.stringify(mInfo);
+    statusLog("Finished");
+    console.log(tf.getBackend());
+};
+
+load();
+
---
+++ mnist-lenet5-tf/mnist.py
@@ -0,0 +1,173 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+LEARNING_RATE = 0.15
+REGULARAZTION_RATE = 0.01
+BATCH_SIZE = 64
+TRAIN_BATCHES = 1500
+TEST_BATCH_SIZE = 1000
+TEST_ITERATION_FREQUENCY = 5
+IMAGE_SIZE = 28
+INPUT_NODE = 784
+OUTPUT_NODE = 10
+NUM_CHANNELS = 1
+
+CONV1_SIZE = 5
+CONV1_DEEP = 6
+CONV2_SIZE = 5
+CONV2_DEEP = 16
+DENSE1_SIZE = 120
+DENSE2_SIZE = 84
+
+
+def inference(input_tensor, regularizer):
+    # conv1
+    with tf.variable_scope("conv1"):
+        conv1_weights = tf.get_variable(
+                "weight", [CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEEP],
+                initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv1_biases = tf.get_variable(
+                "bias", [CONV1_DEEP], 
+                initializer=tf.constant_initializer(0.0))
+        conv1 = tf.nn.conv2d(
+                input_tensor, conv1_weights, strides=[1, 1, 1, 1], padding="SAME")
+        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+
+    # pool1
+    with tf.variable_scope("pool1"):
+        pool1 = tf.nn.max_pool(
+                relu1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+
+    # conv2
+    with tf.variable_scope("conv2"):
+        conv2_weights = tf.get_variable(
+                "weight", [CONV2_SIZE, CONV2_SIZE, CONV1_DEEP, CONV2_DEEP],
+                initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv2_biases = tf.get_variable(
+                "bias", [CONV2_DEEP], 
+                initializer=tf.constant_initializer(0.0))
+        conv2 = tf.nn.conv2d(
+                pool1, conv2_weights, strides=[1, 1, 1, 1], padding="VALID")
+        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+
+    # pool2
+    with tf.variable_scope("pool2"):
+        pool2 = tf.nn.max_pool(
+                relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+
+    # get pool shape
+    pool_shape = pool2.get_shape().as_list()
+    print("pool_shape")
+    print(pool_shape)
+    
+    # flatten
+    nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
+    reshaped = tf.reshape(pool2, [tf.shape(pool2)[0], nodes])
+
+    # dense1
+    with tf.variable_scope("dense1"):
+        dense1_weights = tf.get_variable(
+                "weight", [nodes, DENSE1_SIZE],
+                initializer=tf.truncated_normal_initializer(stddev=0.1))
+        dense1_biases = tf.get_variable(
+                "bias", [DENSE1_SIZE],
+                initializer=tf.constant_initializer(0.0))
+        if regularizer is not None:
+            tf.add_to_collection("losses", regularizer(dense1_weights))
+        dense1 = tf.nn.relu(
+                tf.matmul(reshaped, dense1_weights) + dense1_biases)
+
+    # dense2
+    with tf.variable_scope("dense2"):
+        dense2_weights = tf.get_variable(
+                "weight", [DENSE1_SIZE, DENSE2_SIZE],
+                initializer=tf.truncated_normal_initializer(stddev=0.1))
+        dense2_biases = tf.get_variable(
+                "bias", [DENSE2_SIZE],
+                initializer=tf.constant_initializer(0.0))
+        if regularizer is not None:
+            tf.add_to_collection("losses", regularizer(dense2_weights))
+        dense2 = tf.nn.relu(
+                tf.matmul(dense1, dense2_weights) + dense2_biases)
+
+    # dense3
+    with tf.variable_scope("dense3"):
+        dense3_weights = tf.get_variable(
+                "weight", [DENSE2_SIZE, OUTPUT_NODE],
+                initializer=tf.truncated_normal_initializer(stddev=0.1))
+        dense3_biases = tf.get_variable(
+                "bias", [OUTPUT_NODE],
+                initializer=tf.constant_initializer(0.0))
+        if regularizer is not None:
+            tf.add_to_collection("losses", regularizer(dense3_weights))
+        dense3 = tf.matmul(dense2, dense3_weights) + dense3_biases
+        
+    return dense3
+
+
+def train(mnist):
+    x = tf.placeholder(
+            tf.float32, [None, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS], name="x-input")
+    y_ = tf.placeholder(
+            tf.float32, [None, OUTPUT_NODE], name="y-input")
+    global_step = tf.Variable(0, trainable=False)
+
+    regularizer = tf.contrib.layers.l2_regularizer(
+            REGULARAZTION_RATE)
+    y = inference(x, regularizer)
+
+    # wrong ?
+    #cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
+    #        logits=tf.cast(tf.argmax(y, 1), tf.float32),
+    #        labels=tf.cast(tf.argmax(y_, 1), tf.float32))
+
+
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits = y,
+        labels = tf.argmax(y_, 1)
+    )
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    loss = cross_entropy_mean #+ tf.add_n(tf.get_collection('losses'))
+    train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).\
+        minimize(loss, global_step=global_step)
+
+    # define validation
+    correction_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correction_prediction, tf.float32))
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        vx = mnist.validation.images
+
+        vx_reshaped = np.reshape(vx, (len(vx),
+                                      IMAGE_SIZE,
+                                      IMAGE_SIZE,
+                                      NUM_CHANNELS))
+        validate_feed = {x: vx_reshaped,
+                         y_: mnist.validation.labels}
+
+        for i in range(TRAIN_BATCHES):
+            # prepare training data
+            xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            reshaped_xs = np.reshape(xs, (BATCH_SIZE,
+                                          IMAGE_SIZE,
+                                          IMAGE_SIZE,
+                                          NUM_CHANNELS))
+            sess.run(train_step, feed_dict={x: reshaped_xs, y_: ys})
+
+            # check validation
+            if i % TEST_ITERATION_FREQUENCY == 0:
+                validate_acc = sess.run(accuracy, feed_dict=validate_feed)
+                print("After %d training step(s), validation accuracy is %g"
+                      % (i, validate_acc))
+
+
+def main(argv=None):
+    mnist = input_data.read_data_sets("./mnist-data", one_hot=True)
+    train(mnist)
+
+
+if __name__ == "__main__":
+    tf.app.run()
+
