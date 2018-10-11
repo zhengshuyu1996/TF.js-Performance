1bf84523146b44ffbdd93eacb156ce403911a8ec
David Xiang <xiangdongwei@pku.edu.cn>
2018-10-11 09:00:25 +0800

    register events to send info to extension

--- mnist-comparison/training/scripts/hyperparams.js
+++ mnist-comparison/training/scripts/hyperparams.js
@@ -1,5 +1,3 @@
-const VERBOSE = true;
-
 const BATCH_SIZE = 64;
 const TRAIN_SIZE = 60000; // 60000
 const TRAIN_BATCHES = TRAIN_SIZE / BATCH_SIZE;
@@ -12,3 +10,8 @@ const OUTPUT_NODE = 10;
 const NUM_CHANNELS = 1;
 
 const LEARNING_RATE = 0.15;
+
+const LOCAL_SERVER = "http://localhost:8000";
+
+const VERBOSE = true;
+const DO_TEST = false;
--- mnist-comparison/training/scripts/ui.js
+++ mnist-comparison/training/scripts/ui.js
@@ -3,3 +3,40 @@ const statusElem = document.getElementById("status");
 function statusLog(status){
     statusElem.innerText = "Status: " + status; 
 }
+
+function registerListener(){
+    document.addEventListener("started", function(e){ 
+        chrome.extension.sendMessage(
+            // 发送的e.detailed是一个obj，有两个属性type, message
+            e.detailed,
+            function(response){
+                // callback
+            }
+        );
+    }, false);
+
+    document.addEventListener("finished", function(d){
+        chrome.extension.sendMessage( 
+            e.detailed,
+            function(response){
+                // callback
+            }
+        );
+    }, false);
+}
+
+function triggerStart(){
+    let event = new CustomEvent("started", {
+        type: "started",
+        message: null
+    });
+    document.dispatchEvent(event);
+}
+
+function triggerEnd(msg){
+    let event = new CustomEvent("finished", {
+        type: "finished",
+        message: msg
+    });
+    document.dispatchEvent(event);
+}
