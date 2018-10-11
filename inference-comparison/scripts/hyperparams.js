8e7181eeaaca2d4936a99ad23dd238a1524c0a85
David Xiang <xiangdongwei@pku.edu.cn>
2018-10-11 18:01:45 +0800

    finish VGG19 on tfjs inference

--- inference-comparison/scripts/hyperparams.js
+++ inference-comparison/scripts/hyperparams.js
@@ -1,15 +1,5 @@
 const VERBOSE = true;
 
 const BATCH_SIZE = 64;
-const TRAIN_SIZE = 60000; // 60000
-const TRAIN_BATCHES = TRAIN_SIZE / BATCH_SIZE;
-const TEST_SIZE = 10000; // 10000
 
-const IMAGE_LENGTH = 28;
-const INPUT_NODE = 784;
-const HIDDEN_SIZE = 128;
-const OUTPUT_NODE = 10;
-const NUM_CHANNELS = 1;
-
-const LEARNING_RATE = 0.15;
 const LOCAL_SERVER = "http://localhost:8000";
---
+++ inference-comparison/scripts/tfjs-gpu-vgg19.js
@@ -0,0 +1,77 @@
+/*
+author: David Xiang
+email: xdw@pku.edu.cn
+ */
+'use strict'
+let model;
+const TASK = "VGG19\ttfjs\tgpu\t"
+async function initData(){
+    let offset = tf.scalar(127.5);
+    let data = [];
+    for (let i = 1; i <= DATA_SIZE; i++){
+        // load img from a canvas
+        let img = document.getElementById("pic"+i);
+        let dataItem = tf.fromPixels(img).
+                        toFloat().
+                        sub(offset).
+                        div(offset).
+                        reshape([1, 224, 224, 3]);
+        data.push(dataItem);
+    }
+    return data;
+}
+async function initModel(){
+    //set backend
+    tf.setBackend("webgl");
+    console.log(tf.getBackend());
+
+    // load model
+    console.log("loading model");
+    statusLog("Loading Model")
+    model = await tf.loadModel(LOCAL_SERVER+"/model/tfjs/vgg19/model.json");
+
+    // warm up the model
+    console.log("warmup");
+    statusLog("Warming up");
+    for (let i = 1; i < 3; i++){
+        console.time("warmup");
+        model.predict(tf.zeros([1, 224, 224, 3])).dispose();
+        console.timeEnd("warmup");
+    }
+}
+
+async function infer(data){
+    triggerStart();
+    statusLog("Inferring");
+
+    let totTime = 0;
+    for (let i = 0; i < TEST_SIZE; i++){
+        if (VERBOSE)
+            console.log("Case" + i);
+
+        let begin = new Date();
+        model.predict(data[i%DATA_SIZE]);
+        let end = new Date();
+        totTime += end - begin;
+    }
+    triggerEnd(TASK + "time:\t" + totTime + "ms\t");
+}
+
+async function init(){
+    registerListener();
+    await initModel();
+
+    let data = initData();
+
+    statusLog("Ready");
+    return data;
+}
+
+
+async function main(){
+    let data = await init();
+    await infer(data);
+    statusLog("Finished");
+}
+main();
+
