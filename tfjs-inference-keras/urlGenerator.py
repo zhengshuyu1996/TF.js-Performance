d65f2267ec4ae44fc957219388b9dc41293bd5bc
David Xiang <xiangdongwei@pku.edu.cn>
2018-10-20 16:15:22 +0800

    update urlGenerator.py

--- tfjs-inference-keras/urlGenerator.py
+++ tfjs-inference-keras/urlGenerator.py
@@ -12,11 +12,21 @@ backend_list = ["cpu", "gpu"]
 testsize = 2000
 
 print("[")
-print('    "",')
 
-for html in html_list:
-    for model in model_list:
-        for backend in backend_list:
-            print('    "%s?model=%s&backend=%s&testsize=%d",' % (html, model, backend, testsize))
+r1 = len(html_list)
+r2 = len(model_list)
+r3 = len(backend_list)
+count = 0
+
+for i in range(r1):
+    for j in range(r2):
+        for k in range(r3):
+            print('    "%s?model=%s&backend=%s&testsize=%d"' % 
+                    (html_list[i], model_list[j], backend_list[k], testsize), end="")
+            if (count != r1 * r2 * r3 - 1):
+                print(",")
+            else:
+                print("")
+            count += 1
 
 print("]")
