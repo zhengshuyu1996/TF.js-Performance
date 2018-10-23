0fd98784eae1e2a7af932959a1d820cb16e93e4b
zhengshuyu1996 <zhengshuyu1996@163.com>
2018-10-23 23:49:44 +0800

    add lstm-training

---
+++ lstm-training/js/lstm.js
@@ -0,0 +1,190 @@
+/**
+ * TensorFlow.js Example: LSTM Text Generation.
+ *
+ * Inspiration comes from:
+ *
+ * -
+ * https://github.com/keras-team/keras/blob/master/examples/lstm_text_generation.py
+ * - Andrej Karpathy. "The Unreasonable Effectiveness of Recurrent Neural
+ * Networks" http://karpathy.github.io/2015/05/21/rnn-effectiveness/
+ */
+
+
+/**
+ * Class that manages LSTM-based text generation.
+ *
+ * This class manages the following:
+ *
+ * - Creating and training a LSTM model, written with the tf.layers API, to
+ *   predict the next character given a sequence of input characters.
+ * - Generating random text using the LSTM model.
+ */
+class LSTMTextGenerator {
+  /**
+   * Constructor of NeuralNetworkTextGenerator.
+   *
+   * @param {TextData} textData An instance of `TextData`.
+   */
+  constructor(textData) {
+    this.textData_ = textData;
+    this.charSetSize_ = textData.charSetSize();
+    this.sampleLen_ = textData.sampleLen();
+    this.textLen_ = textData.textLen();
+  }
+
+  /**
+   * Create LSTM model from scratch.
+   *
+   * @param {number | number[]} lstmLayerSizes Sizes of the LSTM layers, as a
+   *   number or an non-empty array of numbers.
+   */
+  createModel(lstmLayerSizes) {
+    if (!Array.isArray(lstmLayerSizes)) {
+      lstmLayerSizes = [lstmLayerSizes];
+    }
+
+    if (backend == "cpu")
+        tf.setBackend("cpu");
+    else
+        tf.setBackend("webgl");
+    if (verbose){
+        console.log(tf.getBackend());
+        console.log("init model");
+    }
+
+    this.model = tf.sequential();
+    for (let i = 0; i < lstmLayerSizes.length; ++i) {
+      const lstmLayerSize = lstmLayerSizes[i];
+      this.model.add(tf.layers.lstm({
+        units: lstmLayerSize,
+        returnSequences: i < lstmLayerSizes.length - 1,
+        inputShape: i === 0 ? [this.sampleLen_, this.charSetSize_] : undefined
+      }));
+    }
+    this.model.add(
+        tf.layers.dense({units: this.charSetSize_, activation: 'softmax'}));
+  }
+
+  /**
+   * Compile model for training.
+   *
+   * @param {number} learningRate The learning rate to use during training.
+   */
+  compileModel(learningRate) {
+    const optimizer = tf.train.rmsprop(learningRate);
+    this.model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
+    console.log(`Compiled model with learning rate ${learningRate}`);
+    this.model.summary();
+  }
+
+  /**
+   * Train the LSTM model.
+   *
+   * @param {number} numEpochs Number of epochs to train the model for.
+   * @param {number} examplesPerEpoch Number of epochs to use in each training
+   *   epochs.
+   * @param {number} batchSize Batch size to use during training.
+   * @param {number} validationSplit Validation split to be used during the
+   *   training epochs.
+   */
+  async fitModel(numEpochs, examplesPerEpoch, batchSize, validationSplit) {
+    let batchCount = 0;
+    const batchesPerEpoch = examplesPerEpoch / batchSize;
+    const totalBatches = numEpochs * batchesPerEpoch;
+
+    onTrainBegin();
+    await tf.nextFrame();
+
+    await triggerStart();
+    let t = new Date().getTime();
+
+    for (let i = 0; i < numEpochs; ++i) {
+      const [xs, ys] = this.textData_.nextDataEpoch(examplesPerEpoch);
+      await this.model.fit(xs, ys, {
+        epochs: 1,
+        batchSize: batchSize,
+        // validationSplit,
+        callbacks: {
+          onBatchEnd: async (batch, logs) => {
+            // Calculate the training speed in the current batch, in # of
+            // examples per second.
+            const t1 = new Date().getTime();
+            const examplesPerSec = batchSize / ((t1 - t) / 1e3);
+            t = t1;
+            onTrainBatchEnd(
+                logs.loss, ++batchCount / totalBatches, examplesPerSec);
+          },
+          onEpochEnd: async (epoch, logs) => {
+            onTrainEpochEnd(logs.val_loss);
+          },
+        }
+      });
+      xs.dispose();
+      ys.dispose();
+    }
+
+    triggerEnd(task + t);
+  }
+
+  /**
+   * Generate text using the LSTM model.
+   *
+   * @param {number[]} sentenceIndices Seed sentence, represented as the
+   *   indices of the constituent characters.
+   * @param {number} length Length of the text to generate, in number of
+   *   characters.
+   * @param {number} temperature Temperature parameter. Must be a number > 0.
+   * @returns {string} The generated text.
+   */
+  async generateText(sentenceIndices, length, temperature) {
+    onTextGenerationBegin();
+    const temperatureScalar = tf.scalar(temperature);
+
+    let generated = '';
+    while (generated.length < length) {
+      // Encode the current input sequence as a one-hot Tensor.
+      const inputBuffer =
+          new tf.TensorBuffer([1, this.sampleLen_, this.charSetSize_]);
+      for (let i = 0; i < this.sampleLen_; ++i) {
+        inputBuffer.set(1, 0, i, sentenceIndices[i]);
+      }
+      const input = inputBuffer.toTensor();
+
+      // Call model.predict() to get the probability values of the next
+      // character.
+      const output = this.model.predict(input);
+
+      // Sample randomly based on the probability values.
+      const winnerIndex = sample(tf.squeeze(output), temperatureScalar);
+      const winnerChar = this.textData_.getFromCharSet(winnerIndex);
+      await onTextGenerationChar(winnerChar);
+
+      generated += winnerChar;
+      sentenceIndices = sentenceIndices.slice(1);
+      sentenceIndices.push(winnerIndex);
+
+      input.dispose();
+      output.dispose();
+    }
+    temperatureScalar.dispose();
+    return generated;
+  }
+};
+
+
+async function main () {
+  let argsStatus = parseArgs(); // defined in params.js
+  if (argsStatus == false)
+      return;
+
+  // let model = await fetchData();
+  // model = await createModel(model);
+  // await trainModel(model);
+
+  fetchData();
+}
+
+main();
+
+
+
---
+++ lstm-training/js/ui.js
@@ -0,0 +1,132 @@
+
+async function triggerStart(){
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // wait for 5 seconds
+    let event = new CustomEvent("started");
+    console.log("start");
+    document.dispatchEvent(event);
+}
+
+function triggerEnd(msg){
+    let event = new CustomEvent("finished", {
+        "detail":{
+            message: msg
+        }
+    });
+    console.log("end");
+    console.log(msg);
+    document.dispatchEvent(event);
+}
+
+
+// UI controls.
+const testText = document.getElementById('test-text');
+const appStatus = document.getElementById('app-status');
+
+const sampleLen = 40;
+const sampleStep = 3;
+
+// Module-global instance of TextData.
+let textData;
+
+// Module-global instance of SaveableLSTMTextGenerator.
+let textGenerator;
+
+function logStatus(message) {
+  appStatus.textContent = message;
+}
+
+let lossValues;
+let batchCount;
+
+/**
+ * A function to call when a training process starts.
+ */
+function onTrainBegin() {
+  lossValues = [];
+  logStatus('Starting model training...');
+}
+
+/**
+ * A function to call when a batch is competed during training.
+ *
+ * @param {number} loss Loss value of the current batch.
+ * @param {number} progress Total training progress, as a number between 0
+ *   and 1.
+ * @param {number} examplesPerSec The training speed in the batch, in examples
+ *   per second.
+ */
+function onTrainBatchEnd(loss, progress, examplesPerSec) {
+  batchCount = lossValues.length + 1;
+  lossValues.push({'batch': batchCount, 'loss': loss, 'split': 'training'});
+  plotLossValues();
+  logStatus(
+      `Model training: ${(progress * 1e2).toFixed(1)}% complete... ` +
+      `(${examplesPerSec.toFixed(0)} examples/s)`);
+}
+
+function onTrainEpochEnd(validationLoss) {
+  lossValues.push(
+      {'batch': batchCount, 'loss': validationLoss, 'split': 'validation'});
+  plotLossValues();
+}
+
+function plotLossValues() {
+  console.log(lossValues[lossValues.length-1])
+}
+
+
+async function fetchData() {
+  let dataIdentifier = 'Nietzsche';
+  await fetch('/Nietzsche.txt')
+    .then (res => {
+      res.text().then(data => {
+        testText.value = data;
+        textData = new TextData(dataIdentifier, testText.value, sampleLen, sampleStep);
+        textGenerator = new LSTMTextGenerator(textData);
+        createModel();
+      })
+    })
+}
+
+
+async function createModel () {
+  if (textGenerator == null) {
+    logStatus('createModel ERROR: Please load text data set first.');
+    return;
+  }
+
+  logStatus('Creating model... Please wait.');
+  for (let i = 0; i < lstmLayerSizes.length; ++i) {
+    const lstmLayerSize = lstmLayerSizes[i];
+    if (!(lstmLayerSize > 0)) {
+      logStatus(
+          `ERROR: lstmLayerSizes must be a positive integer, ` +
+          `but got ${lstmLayerSize} for layer ${i + 1} ` +
+          `of ${lstmLayerSizes.length}.`);
+      return;
+    }
+  }
+
+  await textGenerator.createModel(lstmLayerSizes);
+  logStatus(
+      'Done creating model. ' +
+      'Now you can train the model or use it to generate text.');
+
+  trainModel()
+}
+
+
+async function trainModel () {
+  if (textGenerator == null) {
+    logStatus('trainModel ERROR: Please load text data set first.');
+    return;
+  }
+  
+  textGenerator.compileModel(learningRate);
+
+  await textGenerator.fitModel(
+      numEpochs, examplesPerEpoch, batchSize, validationSplit);
+
+}
+
